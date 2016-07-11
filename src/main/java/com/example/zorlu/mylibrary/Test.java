f04afe63b0d2e96b74829087e0379725597ea22b
amerty95 <amerty95@hotmail.com>
2016-07-11 13:27:18 +0300

    -

--- src/main/java/com/example/zorlu/mylibrary/Test.java
+++ src/main/java/com/example/zorlu/mylibrary/Test.java
@@ -1,10 +1,22 @@
 package com.example.zorlu.mylibrary;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
 /**
  * Created by ZORLU on 30.06.2016.
  */
 public class Test {
-    public static String getString() {
-        return "no tag s11";
+    public static String getString(Context aa) {
+
+        PackageInfo pInfo = null;
+        try {
+            pInfo = aa.getPackageManager().getPackageInfo(aa.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        String version = pInfo.versionName;
+        return version;
     }
 }
