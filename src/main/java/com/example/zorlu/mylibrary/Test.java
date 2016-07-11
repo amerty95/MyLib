43af1202a2474908a0e2ba357eca38c054b22c5b
amerty95 <amerty95@hotmail.com>
2016-07-11 13:52:32 +0300

    v14

--- src/main/java/com/example/zorlu/mylibrary/Test.java
+++ src/main/java/com/example/zorlu/mylibrary/Test.java
@@ -8,15 +8,10 @@ import android.content.pm.PackageManager;
  * Created by ZORLU on 30.06.2016.
  */
 public class Test {
-    public static String getString(Context aa) {
+    public static String getString() {
 
-        PackageInfo pInfo = null;
-        try {
-            pInfo = aa.getPackageManager().getPackageInfo(aa.getPackageName(), 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        String version = pInfo.versionName;
+
+        String version = "test123";
         return version;
     }
 }
